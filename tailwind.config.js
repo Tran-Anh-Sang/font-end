9182af76152a6d55693248bffb7c2b3d2b7b8af4
Tran-Anh-Sang <164865450+Tran-Anh-Sang@users.noreply.github.com>
2024-12-25 16:07:52 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/**/*.{html,js,jsx,ts,tsx}', // Cập nhật phần này
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config;
